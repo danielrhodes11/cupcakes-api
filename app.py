1409fb6d3f7f4f25569715d138251bf3064878f1
Daniel Rhodes <danielrhodes47@gmail.com>
2023-08-14 08:07:36 -0500

    added get/posts requests

--- app.py
+++ app.py
@@ -11,10 +11,33 @@ app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql:///cupcakes"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SQLALCHEMY_ECHO'] = True
 
+debug = DebugToolbarExtension(app)
+
 connect_db(app)
 
 
-@app.route('/')
-def home_page():
-    """Render homepage"""
-    return render_template('index.html')
+@app.route('/api/cupcakes')
+def all_cupcakes():
+    """Get data about all cupcakes."""
+
+    cupcakes = [cupcake.serialize() for cupcake in Cupcake.query.all()]
+    return jsonify(cupcakes=cupcakes)
+
+
+@app.route('/api/cupcakes', methods=["POST"])
+def create_cupcake():
+    """Create a cupcake with flavor, size, rating and image data from the body of the request. Returns JSON of the newly created cupcake."""
+
+    new_cupcake = Cupcake(flavor=request.json["flavor"], size=request.json["size"],
+                          rating=request.json["rating"], image=request.json["image"])
+    db.session.add(new_cupcake)
+    db.session.commit()
+    return (jsonify(cupcake=new_cupcake.serialize()), 201)
+
+
+@app.route('/api/cupcakes/<int:id>')
+def get_cupcake(id):
+    """Get data about a single cupcake."""
+
+    cupcake = Cupcake.query.get_or_404(id)
+    return jsonify(cupcake=cupcake.serialize())
--- models.py
+++ models.py
@@ -11,3 +11,47 @@ def connect_db(app):
     db.app = app
     db.init_app(app)
     app.app_context().push()
+
+
+class Cupcake(db.Model):
+    """Cupcake."""
+
+    __tablename__ = 'cupcakes'
+
+    id = db.Column(db.Integer,
+                   primary_key=True,
+                   autoincrement=True)
+
+    flavor = db.Column(db.Text,
+                       nullable=False)
+
+    size = db.Column(db.Text,
+                     nullable=False)
+
+    rating = db.Column(db.Float,
+                       nullable=False)
+
+    image = db.Column(db.Text,
+                      nullable=False,
+                      default=DEFAULT_IMAGE)
+
+    def __repr__(self):
+        """Show info about cupcake."""
+
+        c = self
+        return f"<Cupcake {c.id} {c.flavor} {c.size} {c.rating} {c.image}>"
+
+    def serialize(self):
+        """Serialize to dictionary."""
+
+        return {
+            "id": self.id,
+            "flavor": self.flavor,
+            "size": self.size,
+            "rating": self.rating,
+            "image": self.image if self.image else DEFAULT_IMAGE
+        }
+
+# q: i got a key error when i didnt include the image in a post request. why?
+# a: because the image is set to default in the model, but not in the post request. so it's not in the request.json. so it's not in the dictionary. so it's not in the serialize method.
+# q: how can i fix this?
